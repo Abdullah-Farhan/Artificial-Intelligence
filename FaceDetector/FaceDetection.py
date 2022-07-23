310589434b29bf1feba35c23a82d3de132dbd4f5
Abdullah-Farhan <102550496+Abdullah-Farhan@users.noreply.github.com>
2022-07-23 08:47:25 -0700

    Update FaceDetection.py

--- FaceDetector/FaceDetection.py
+++ FaceDetector/FaceDetection.py
@@ -9,12 +9,12 @@ face_coordinates = face_data.detectMultiScale(gray_img)
 
 #face coordinates generates a 2D array and each array in it contains the coordinates of a face detected
 for i in face_coordinates:
-    x = i[0]
-    y = i[1]
-    a = i[2]
-    b = i[3]
+    x_coordinate = i[0]
+    y_coordinate = i[1]
+    width = i[2]
+    height = i[3]
     #Drawing a rectangle on each face detected
-    cv2.rectangle(img, (x, y), ((a+x), (y+ b)), (0, 255, 0), 2)
+    cv2.rectangle(img, (x_coordinate, y_coordinate), ((x_coordinate + width), (y_coordinate + height)), (0, 255, 0), 2)
 
 cv2.imshow('Face Detector', img)
 cv2.waitKey()
